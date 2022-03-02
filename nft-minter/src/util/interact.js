d1dab610d315cde3e0fa8c478b552062872b9281
Ajay Kumar <ajaykumarmail@gmail.com>
2022-03-02 23:55:45 +0530

    minor

--- nft-minter/src/util/interact.js
+++ nft-minter/src/util/interact.js
@@ -2,7 +2,7 @@ import { pinJSONToIPFS } from "./pinata.js";
 require("dotenv").config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const contractABI = require("../contract-abi.json");
-const contractAddress = "0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE";
+const contractAddress = "0x7c97666d1d898684757F1DF14585a647Ca0c5D65";
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 
